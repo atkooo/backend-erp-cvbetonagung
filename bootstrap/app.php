8588af5db681743aaf532fd806610a9ef228f56d
Okta Maulana <okta.maulana12@gmail.com>
2026-07-03 11:06:31 +0700

    feat: initialize backend project configuration with environment support and custom middleware definitions

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*');
         $middleware->alias([
             'api.token' => AuthenticateApiToken::class,
             'permission' => RequirePermission::class,
